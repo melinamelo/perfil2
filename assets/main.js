5e40fdc20640ac04313803fb8f6eee2de33d4abf
melinamelo <melinamelo0688@gmail.com>
2023-08-04 10:38:00 -0300

    Fazer mudar de cores varias vezes

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,16 @@
 function cor( e ) {
-    document.body.style.backgroundColor = 'antiquewhite'
+    let teste = generateColor();
+    //document.body.style.backgroundColor = 'antiquewhite'
+    document.body.style.backgroundColor = teste;
 }
+function generateColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    
+    return color;
+    
+  }
